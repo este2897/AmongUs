9fd91684b727f644caf71130d164315a3e21dca6
AlbertoGC9 <71413384+AlbertoGC9@users.noreply.github.com>
2020-11-19 15:41:37 -0600

    Add files via upload
    Version final 1.1

--- P1-SimonDice.c
+++ P1-SimonDice.c
@@ -99,7 +99,111 @@ void main(){
 		if(simon[i]==lectura && i==cont){
 			simon[i+1]=aleatorio;
 		}
-		
+		if(simon[i]!=lectura){
+			error=1;
+		}
+		i++;
+	}
+	while(error==1 && !(bit_test(PORTB,4))){
+		bit_set(PORTD,0);
+		bit_set(PORTD,1);
+		bit_set(PORTD,2);
+		bit_set(PORTD,3);
+	}
+
+	if(error==1){
+		LATD=0X00;
+		cont=0;
+		error=0;
+		conteo=0;
+		delay_ms(1000);
+		i=0;
+	}
+
+	else{
+		error=0;
+		i=0;
+		cont++;
+		conteo++;
+	}
+	cont++;
+	conteo++;
+}
+
+void configuracion(){
+	setup_wdt(WDT_OFF);
+	setup_comparator(NC_NC_NC_NC);
+	setup_spi(FALSE);
+	setup_low_volt_detect(FALSE);
+	setup_oscillator(False);
+
+	setup_adc_ports(NO_ANALOGS|VSS_ADD);
+	setup_adc(ADC_OFF);
+	setup_vref(FALSE);
+
+	set_rtcc((int16)65067);
+	setup_timer_0(RTCC_INTERNAL|RTCC_DIV_256);
+	enable_interrupts(INT_RTCC);
+	enable_interrupts(GLOBAL);
+
+	setup_timer_1(T1_DISABLED);
+
+	setup_timer_2(T2_DISABLED,0,1);
+
+	setup_timer_3(T3_DISABLED|T3_DIV_BY_1);
+
+	TRISB=0X1F;
+	LATB=0X00;
+	PORTB=0X00;
+
+	TRISC=0X00;
+	LATC=0X00;
+	PORTC=0X00;
+
+	TRISA=0XF0;
+	LATA=0X00;
+	PORTA=0X00;
+
+	TRISD=0X00;
+	LATD=0X00;
+	PORTD=0X00;
+
+	TRISE=0XFF;
+	LATE=0X00;
+	PORTE=0X00;
+}
+
+void imprimir(){
+	
+	for(j=0; j<=cont; j++){
+		PORTD=simon[j];
+		delay_ms(250);
+		PORTD=0X00;
+		delay_ms(250);
 	}
 }
 
+void display(){
+
+	if(cdisplay==0){
+		OFF(UNI);
+		ON(DEC);
+		estritura=conteo/10l
+		escritura=(escritura*16)|(PORTD & 0X0F);
+		LATD=(escritura);
+	}
+
+	if(cdisplay==1){
+		ON(UNI);
+		OFF(DEC);
+		escritura=conteo % 10;
+		escritura=(escritura*16)|(PORTD & 0X0F);
+		LATD=(escritura);
+	}
+
+	cdisplay++;
+
+	if(cdisplay==2){
+		cdisplay=0;
+	}
+}
