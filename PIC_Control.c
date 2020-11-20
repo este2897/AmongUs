22e213f7f18d5b7d4e39f1bce8bbb36dd73cf201
esmontero <70980649+esmontero@users.noreply.github.com>
2020-11-20 10:12:49 -0600

    Update PIC_Control.c

--- PIC_Control.c
+++ PIC_Control.c
@@ -10,6 +10,27 @@
 #include "configuracion.h"
 #include "USART_libreria.h"
 
+void ADCInit()
+{
+    TRISA = 0x00000001;		/*Set as input port*/
+    ADCON1 = 0x0e;  //AN0 entrada analogica
+    ADCON2 = 0x92;  //configuracion ADC
+    ADRESH=0;  		//limpiar registros
+    ADRESL=0;
+}
+
+int ADCRead(int canal)
+{
+    unsigned int ADCin;
+    ADCON0 =(ADCON0 & 0b11000011)|((canal<<2) & 0b00111100); //selecciona canal ADC
+    ADCON0 |= ((1<<ADON)|(1<<GO));//Inicia ADC
+    while(ADCON0bits.GO_nDONE==1);//Espera que termine ADC
+    //ADCin = (ADRESH<<8+ADRESL);//Escribe el valor de ADC
+    ADCin = (ADRESH*256) | (ADRESL);
+    return(ADCin);
+}
+
+
 void main(void) {
 
     USART_Init(9600);
@@ -33,22 +54,29 @@ void main(void) {
 
          // Check de la Entrada a juego 2: apaga luces
         if (PORTBbits.RB0 == 1){
-            correcto = 0;
-            while(correcto == 0){
+            while(PORTBbits.RB0 == 1){
                 USART_Tx(PORTBbits.RB0); //señal para iniciar display juego
 
                 //envia PORTA(lo que el usuario ingresa) al PIC_display
                 USART_Tx(PORTA);
 
-                correcto = USART_Rx() //ya se completo la tarea
+                PORTBbits.RB0 =  ∼USART_Rx() //ya se completo la tarea
             }
         }
-
         // Check del juego 3: PWM
         if (PORTBbits.RB1 == 1){
+            int n=0;
+            int ADCin;
+            OSCCON=0x72;//frecuencia oscilador
+            ADCInit();//inicia ADC
+            USART_Tx(0xFF);
             while(PORTBbits.RB1 == 1){
-            //Para cerrar este while: LATAbits.LA4 = 0;
-
+                ADCin=ADCRead(0);
+                USART_Tx(ADCin);
+                n=USART_Rx();
+                if(n==1){
+                    LATAbits.LA4 = 0;
+                }
             }
         }
     }
